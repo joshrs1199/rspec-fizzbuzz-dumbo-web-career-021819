625065e36b12951a4702d0fd00af9bd1a79c01b3
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-08 23:43:40 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,4 +4,10 @@ def fizzbuzz(number)
   if number % 3 == 0
     return "Fizz"
   end
+  if number % 5 == 0
+    return "Buzz"
+  end
+  if number % 3 == 0 && number % 5 == 0
+    return "FizzBuzz"
+  end
 end
